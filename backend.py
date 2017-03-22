a7a968d5ed559f5ca12b6966ba59069e615bf64e
root <root@ip-172-31-9-17.us-west-2.compute.internal>
2017-03-22 14:20:26 +0000

    more devops

--- backend.py
+++ backend.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import redis
 import random
 import json
--- installServices.sh
+++ installServices.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -ue
 cp transvis-db.service /lib/systemd/system/
+cp transvis-backend.service /lib/systemd/system/
 systemctl enable transvis-db.service
 systemctl start transvis-db.service
+systemctl enable transvis-backend.service
+systemctl start transvis-backend.service
